4bb49d9ed463d63e6de97630a5a998745b9cf412
Enko14 <enkooo14@gmail.com>
2017-04-16 23:31:20 +0300

    Update Menu.java

--- src/main/java/onemore/Menu.java
+++ src/main/java/onemore/Menu.java
@@ -11,7 +11,25 @@ import org.xml.sax.SAXException;
 public class Menu {
     public static void main(String[] args) throws SAXException, TransformerException, JAXBException {
     	Main main = new Main();
-    	AddElement main2 = new AddElement();
+//    	AddElement main2 = new AddElement();
     	main.ReadFile("src/NewFile.xml");
-//		AddElement.searchBook(10, AddElement.allbooks);
+		//		AddElement.searchBook(10, AddElement.allbooks);
+    	main.getAll(); 
+    	System.out.println( "\n----------------");
+    	main.searchAuthor(2);
+    	System.out.println( "\n----------------");
+    	main.searchBook(1); 
+    	System.out.println( "\n----------------");
+    	main.addAuthor(3, "Алексей", 1807);
+    	System.out.println( "\n----------------");
+//    		main.getBooksByAuthor2(2);
+    	main.showAuthor(main.getAuthors(1));
+    	System.out.println( "\n----------------");
+    	main.BooksList();
+    	System.out.println( "\n----------------");
+    	main.AuthorsList();
+    	System.out.println( "\n----------------");
+    	main.saveToFile("src/NewFile.xml");
+    	System.out.println( "\n----------------");
+    
 }}
