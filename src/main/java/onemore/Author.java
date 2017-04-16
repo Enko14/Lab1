f302186151f2dae61810b7c4f45febc95146ef97
Enko14 <enkooo14@gmail.com>
2017-04-16 23:30:35 +0300

    Update Author.java

--- src/main/java/onemore/Author.java
+++ src/main/java/onemore/Author.java
@@ -3,44 +3,62 @@ package onemore;
 import java.util.List;
 import javax.xml.bind.annotation.*;
 
-
 //@XmlType(propOrder = {"id","name","year_of_birth","authors"})
 @XmlAccessorType(XmlAccessType.FIELD)
-@XmlSeeAlso({Author.class})
+@XmlSeeAlso({ Author.class })
 public class Author {
-	@XmlAttribute (name="id")
+	
+	@XmlAttribute(name = "id")
 	public int id;
-	@XmlAttribute (name="name")
+	@XmlAttribute(name = "name")
 	public String name;
-	@XmlAttribute (name="birthdate")
+	@XmlAttribute(name = "birthdate")
 	public int birthdate;
-	@XmlElementWrapper(name="books")
-	@XmlElement (name="book")
-	public List<Book> books;
+	@XmlElementWrapper(name = "books")
+	@XmlElement(name = "book")
+	public List<Book> books;		
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public int getBirthdate() {
 		return birthdate;
 	}
+
 	public void setBirthdate(int birthdate) {
 		this.birthdate = birthdate;
 	}
+
 	public List<Book> getBooks() {
 		return books;
 	}
+
 	public void setBooks(List<Book> books) {
 		this.books = books;
 	}
-	
+
+	public Author(int inId, String inName, int inBirthdate, List<Book> inBooks) {
+		super();
+		id = inId;
+		name = inName;
+		birthdate = inBirthdate;
+		books = inBooks;
+	}
+
+	public Author() {
+		// TODO Auto-generated constructor stub
+	}
 
 }
